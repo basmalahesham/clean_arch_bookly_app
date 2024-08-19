17881da14e227d4dbd6d98f7ae3efd152787a53f
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-20 02:19:57 +0300

    refactor to use case class create use_case.dart Generic use case

---
+++ lib/core/use_cases/use_case.dart
@@ -0,0 +1,7 @@
+import 'package:clean_arch_bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Param> {
+  Future<Either<Failure, Type>> call([Param param]);
+}
+class NoParam {}
--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,18 @@
 import 'package:clean_arch_bookly_app/core/errors/failures.dart';
+import 'package:clean_arch_bookly_app/core/use_cases/use_case.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase{
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
   FetchFeaturedBooksUseCase(this.homeRepo);
-  Future<Either<Failure,List<BookEntity>>> fetchFeatureBooks(){
-    //check permission
-    return homeRepo.fetchFeatureBooks();
-  }
 
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeatureBooks();
+  }
 }
+
+
+
