7ad0c39bcb6a2986a6e6caef5a00683ddc201dd6
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-21 00:09:39 +0300

    create home_repo_impl.dart

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,19 +1,42 @@
 import 'package:clean_arch_bookly_app/core/errors/failures.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeatureBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchFeatureBooks();
+      if (booksList.isNotEmpty){
+        return right(booksList);
+      }
+      var books = await homeRemoteDataSource.fetchFeatureBooks();
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async{
+    try {
+      var booksList = homeLocalDataSource.fetchNewestBooks();
+      if (booksList.isNotEmpty){
+        return right(booksList);
+      }
+      var books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
-
 }
