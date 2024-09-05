62adb06d5f6cf026a51a844cfffe984074414d28
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-05 04:26:04 +0300

    pagination part 3

--- lib/features/home/presentation/views/widgets/feature_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/feature_books_list_view.dart
@@ -1,33 +1,65 @@
 import 'package:clean_arch_bookly_app/core/utils/app_router.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-class FeatureBooksListView extends StatelessWidget {
+class FeatureBooksListView extends StatefulWidget {
   const FeatureBooksListView({super.key, required this.books});
 
   final List<BookEntity> books;
+
+  @override
+  State<FeatureBooksListView> createState() => _FeatureBooksListViewState();
+}
+
+class _FeatureBooksListViewState extends State<FeatureBooksListView> {
+  late final ScrollController _scrollController ;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    var currentPosition = _scrollController.position.pixels;
+    var maxScrollLength = _scrollController.position.maxScrollExtent;
+    if (currentPosition >= 0.7 * maxScrollLength) {
+      BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
+        controller: _scrollController,
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        itemCount: books.length,
+        itemCount: widget.books.length,
         itemBuilder: (context, index) => Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: GestureDetector(
             onTap: () {
               GoRouter.of(context).push(
                 AppRouter.kBookDetailsView,
-                extra: Model(books, index),
+                extra: Model(widget.books, index),
               );
             },
             child: CustomBookImage(
-              imageUrl: books[index].image ?? '',
+              imageUrl: widget.books[index].image ?? '',
             ),
           ),
         ),
